56fb94f5948b3d6ebaf2b455d82e8ce85fdc61c5
John Wesely <john.wesely@tappnetwork.com>
2024-03-26 11:40:26 -0600

    overhaul survey settings edit functionality

--- src/Resources/SurveyResource/Pages/EditSurvey.php
+++ src/Resources/SurveyResource/Pages/EditSurvey.php
@@ -5,6 +5,7 @@ namespace Tapp\FilamentSurvey\Resources\SurveyResource\Pages;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Tapp\FilamentSurvey\Resources\SurveyResource;
+use Illuminate\Database\Eloquent\Model;
 
 class EditSurvey extends EditRecord
 {
@@ -24,5 +25,35 @@ class EditSurvey extends EditRecord
         return [
             Actions\LocaleSwitcher::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $settings = $this->record->settings;
+
+        if ($settings) {
+            $data['limit'] = $settings['limit-per-participant'] > 0;      
+            $data['limit_per_participant'] = $settings['limit-per-participant'] ?? null;
+            $data['allow_guests'] = $settings['accept-guest-entries'];
+        }
+
+        $data['name'] = $this->record->name;
+
+        return $data;
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $settings = [
+            'accept-guest-entries' => $data['allow_guests'],
+            'limit-per-participant' => !$data['limit'] ? -1 : $data['limit_per_participant'],
+        ];
+
+        $record->update([
+            'name' => $data['name'],
+            'settings' => $settings,
+        ]);
+ 
+        return $record;
     }
 }
